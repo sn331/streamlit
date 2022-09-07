f32aaa2864c63c07a99a375e012fd0169933a391
sn331 <113004414+sn331@users.noreply.github.com>
2022-09-07 08:53:10 +0100

    Update dashboard_streamlit_siteb.py

--- dashboard_streamlit_siteb.py
+++ dashboard_streamlit_siteb.py
@@ -64,7 +64,7 @@ df['Year'] = df.index.year
 
 total_sales = df[options].sum()
 #st.write('Total Sales =', total_sales.values)
-st.metric(label="Total Sales", value=total_sales.sum())
+st.metric(label="Total Sales", value=total_sales.values)
 
 st.line_chart(df[options])
 
