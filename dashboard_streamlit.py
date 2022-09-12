d720ca6e946daae99c719fab0d55ff3c0c298bd7
sn331 <113004414+sn331@users.noreply.github.com>
2022-09-12 20:01:47 +0100

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -78,8 +78,7 @@ st.metric(label="Total Sales", value=int(total_sales.sum()))
 st.subheader('Daily Sales Forecast')
 st.line_chart(df[options])
 
-df_cat = df.reset_index()
-df_cat = df.groupby(df['Year', 'Month'])[options].sum()
+df_cat = df.groupby(['Year', 'Month'])[options].sum()
 
 st.subheader('Monthly Sales Forecast')
 st.bar_chart(df_cat)
