6f78a503c994e9f6c4b94bfeb095bb1fc4b2bc8d
sn331 <113004414+sn331@users.noreply.github.com>
2022-09-12 18:34:30 +0100

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -92,14 +92,24 @@ site_A_cat_2021 = site_A_cat[site_A_cat.loc[:, 'Year'] == 2021]
 site_A_cat_2021= site_A_cat_2021.groupby(['Month']).sum()
 site_A_cat_2021 = site_A_cat_2021.reset_index()
 
+df_2022 = df[df['Year'] == 2022]
+df_2022= df_2022.groupby(['Month']).sum()
+df_2022 = df_2022.reset_index()
+
+df_2023 = df[df['Year'] == 2023]
+df_2023= df_2023.groupby(['Month']).sum()
+df_2023 = df_2023.reset_index()
+
 for i in options:
     plt.clf()
     if i == 'Total':
         plt.bar(site_A_cat_2020['Month'], site_A_cat_2020[i].squeeze(), 0.35, alpha = 0.8, label = i + ' (2020)')
         plt.bar(site_A_cat_2021['Month'] + 0.35, site_A_cat_2021[i].squeeze(), 0.35, alpha = 0.8, label = i +' (2021)')
+        plt.bar(df_2022['Month'] + 0.7, df_2022[i].squeeze(), 0.35, alpha = 0.8, label='Forecast (2022)')
     else:
         plt.bar(site_A_cat_2020['Month'], site_A_cat_2020.loc[:, (slice(None), i)].squeeze(), 0.35, alpha = 0.8, label = i + ' (2020)')
         plt.bar(site_A_cat_2021['Month'] + 0.35, site_A_cat_2021.loc[:, (slice(None), i)].squeeze(), 0.35, alpha = 0.8, label = i +' (2021)')
+        plt.bar(df_2022['Month'] + 0.7, df_2022[i].squeeze(), 0.35, alpha = 0.8, label='Forecast (2022)')
     ax = plt.subplot(111)
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
